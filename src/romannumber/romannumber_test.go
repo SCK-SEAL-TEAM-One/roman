61df078581259ccb839f02fb2ea1bdf1f1207525
teerawat.th <teerawat.th@baac.or.th>
2018-07-16 09:42:05 +0700

    [Createdเพิ่ม Test Case

--- src/romannumber/romannumber_test.go
+++ src/romannumber/romannumber_test.go
@@ -14,3 +14,16 @@ func Test_RomanCalculate_Input_FirstNumber_XX_And_SecondNumber_II_Should_Be_XXII
 		t.Errorf("expected %s but got %s", expected, actual)
 	}
 }
+
+func Test_RomanCalculate_Input_FirstNumber_II_And_SecondNumber_II_Should_Be_IV(t *testing.T) {
+	firstNumber := "II"
+	secondNumber := "II"
+
+	expected := "IV"
+
+	actual := RomanCalculate(firstNumber, secondNumber)
+
+	if expected != actual {
+		t.Errorf("expected %s but got %s", expected, actual)
+	}
+}
